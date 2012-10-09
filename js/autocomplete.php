652c66b19960738593470be83d6159aad0d669be
obalco <obal.tom@gmail.com>
2012-10-09 21:54:21 +0200

    prisposobil som autocomplete.php pre novy strukturu databazy

--- js/autocomplete.php
+++ js/autocomplete.php
@@ -5,14 +5,14 @@ include('/../db.php');
  $q=strtolower($_GET['q']);
  $my_data=mysql_real_escape_string($q);
 
- $sql="SELECT staznost_na FROM staznosti WHERE staznost_na LIKE '%$my_data%'";
+ $sql="SELECT who FROM claims WHERE who LIKE '%$my_data%'";
  $result = mysql_query($sql);
  $poc=mysql_num_rows($result);
  
 
 	 if($poc>0){
 		  while($row=mysql_fetch_array($result)){
-		   echo $row['staznost_na']."\n";
+		   echo $row['who']."\n";
 		  }
 	 }
 
