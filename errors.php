9b629a0a0e3cf3a4488802baf52b2a6c4cb84f13
obalco <obal.tom@gmail.com>
2012-10-21 19:57:41 +0200

    vsetko od ada

--- errors.php
+++ errors.php
@@ -3,16 +3,5 @@
 $error=array(
 		 1=>'Dåka textu <b>kto/co</b> Vás napálil, musí obsahova  3 - 20 znakov.<br />',
 		 2=>'Dåka textu <b>èím</b> Vás napálil<b>, musí obsahova  5 - 200 znakov.<br />',
-		 3=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		 4=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		 5=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		 6=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		 7=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		 8=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		 9=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		10=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		11=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-		12=>'Dåka textu <b>kto Vás napálil<b>, musí obsahova  3 - 20 znakov.<br />',
-
 			)
 ?>
